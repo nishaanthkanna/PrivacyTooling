f44c286b0b15c39479efbf7c67a90f6d754069d1
Nishaanth Kanna <nishaanth1998@gmail.com>
2022-12-13 16:40:11 -0400

    Added few variables for logging across multiple GPUs

--- run_ae.py
+++ run_ae.py
@@ -11,13 +11,14 @@ args = parser.parse_args()
 # Currently fixing seed size and enabling deterministic torch
 
 BATCH_SIZE = 128
-EPOCHS = 200
+EPOCHS = 100
 PROTECTED_CLASS = "2 3"
 LR = 4e-4
+GPU = "V100"
 
 for i in range(args.runs):
-    command = f"python3 -m resnet18.py --ckpt_folder ./logs_resnet18_cifar10_default_{i}/ --lr {LR} --protected_class {PROTECTED_CLASS} --batch_size {BATCH_SIZE} --epochs {EPOCHS}"
-    print(f"Running {command} Count: {i}")
+    command = f"python -m resnet18.py --ckpt_folder ./logs_resnet18_cifar10_default_{GPU}_{i}/ --lr {LR} --protected_class {PROTECTED_CLASS} --batch_size {BATCH_SIZE} --epochs {EPOCHS} --deterministic --deterministic_torch"
+    print(f"Running {command} \n Run: {i} ---------")
     train_args = shlex.split(command)
     p = subprocess.Popen(train_args)
     p.wait()
